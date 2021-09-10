2d6a2ae54c9903adc4bd47c65b0fc5e875e4d4ff
setht99 <seth_teng@hotmail.com>
2021-09-10 18:09:41 +0800

    Update classification.s
    ABLE TO LOAD VALUE OF M TO R12 LETS GOOOO

--- EE2028 Lab/EE2028_AY2122S1_Assign1/Src/classification.s
+++ EE2028 Lab/EE2028_AY2122S1_Assign1/Src/classification.s
@@ -35,42 +35,9 @@ classification:
 	PUSH {R0-R6,R8-R12}
 	LDR R5,[R0] @Load x-coordinate of point to r5.
 	LDR R6,[R1] @Load x-coodinate of centro to R6.
-	@MOV R6, #0
-	@MOV R7, #0
-	@MOV R8, #0
-	@LDR R12, =M
-	@LDR R11,[R12]
-	MOV R12, #8
+	LDR R12,[R3] @Load value of M to R12 as counter.
 	BL SUBROUTINE
-/*/ 1st centroid
-	SUB R8,R5,R6  @ (R5 - R6) goes to R8
-	MUL R8,R8 @Squaring x coordinate
-	LDR R5,[R0,#4]! @ R0 jump to next 4 bytes, then load the content to R5
-	LDR R6,[R1,#4]! @ R1 jump to next 4 bytes, then load the content to R6
-	SUB R9,R5,R6 @ (R5 - R6) goes to R9
-	MUL R9,R9 @squaring y coordinate
-	ADD R10,R8,R9 @adding x^2 and y^2 coordinate
-	LDR R5,[R0,#-4]! @R0 go back 4 bytes. x-coordinate
 
-// 2nd centroid
-	LDR R6,[R1,#4]! @R1 jump to next 4 bytes. x-coor of 2nd centroid.
-	SUB R8,R5,R6  @ (R5 - R6) goes to R8
-	MUL R8,R8 @Squaring x coordinate (2nd centroid)
-	LDR R5,[R0,#4]! @ R0 jump to next 4 bytes, then load the content to R5
-	LDR R6,[R1,#4]! @ R1 jump to next 4 bytes, then load the content to R6
-	SUB R9,R5,R6 @ (R5 - R6) goes to R9
-	MUL R9,R9 @squaring y coordinate
-	ADD R11,R8,R9 @adding x^2 and y^2 coordinate
-
-// Comparing and then classifying to 1 or 2.
-	MOV R5,#1
-	MOV R6,#2
-	CMP R10,R11
-	ITE CS
-	STRCS R5,[R2],#4
-	STRCC R6,[R2],#4
-	SUB R12,#1
-```	@BL SUBROUTINE */
 	POP {R0-R6,R8-R12}
 	POP {R14}
 	BX LR
